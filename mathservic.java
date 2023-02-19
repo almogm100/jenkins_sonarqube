8707a04421c25eca8c5ceafd1d23366af4bdf5fd
almogm100 <121858493+almogm100@users.noreply.github.com>
2023-02-19 16:48:34 +0200

    Update mathservic.java

--- mathservic.java
+++ mathservic.java
@@ -1,14 +1,18 @@
+# Python program to multiply all values in the
+# list using traversal
 
-public class MathService {
-    
-    public int multiply(int firstNumber, int secondNumber) {
-        return firstNumber * secondNumber;
-    }
-    
-    public int subtract(int firstNumber, int secondNumber) {
-        Integer myInt = null;
-        A Integer yourInt = myInt * 2;
-        return firstNumber - secondNumber;
-    }
 
-}
+def multiplyList(myList):
+
+	# Multiply elements one by one
+	result = 1
+	for x in myList:
+		result = result * x
+	return result
+
+
+# Driver code
+list1 = [1, 2, 3]
+list2 = [3, 2, 4]
+print(multiplyList(list1))
+print(multiplyList(list2))
